9be63d8047a191243ed9db20354f9f1b1a461770
Junzi Sun <junzisun@gmail.com>
2024-05-04 21:22:12 +0200

    update model format, and minor code fix

--- acropole/predictor.py
+++ acropole/predictor.py
@@ -9,11 +9,17 @@ class Predictor:
     """
     Class to use fuel flow model
     """
-    def __init__(self, default_model="models/Dense_Acropole_FuelFlow_Scaling", mock_tensorflow=None,
-                 mock_pkg_resources=None,
-                 mock_utils=None):
+
+    def __init__(
+        self,
+        default_model="models/acropole_sequential_fuelflow.keras",
+        mock_tensorflow=None,
+        mock_pkg_resources=None,
+        mock_utils=None,
+    ):
         """
         Class initilizer that load dependant packages, and inits model cache.
+
         :param default_model: default model name as a String
         :param mock_tensorflow: mock for tensorflow package (Default is None)
         :param mock_pkg_resources: mock for pkg_resources package (Default is None)
@@ -23,8 +29,12 @@ class Predictor:
 
         self.np = importlib.import_module("numpy")
 
-        self.fuel_max = self.np.array([1, 5000, 50, 50, 600, 50000, 800, 50000, 800, 800, 5000, 1])
-        self.fuel_min = self.np.array([0, -5000, -50, -50, 0, 0, 200, 0, 200, 200, -5000, 0])
+        self.fuel_max = self.np.array(
+            [1, 5000, 50, 50, 600, 50000, 800, 50000, 800, 800, 5000, 1]
+        )
+        self.fuel_min = self.np.array(
+            [0, -5000, -50, -50, 0, 0, 200, 0, 200, 200, -5000, 0]
+        )
 
         if mock_tensorflow is None:
             self.tf = importlib.import_module("tensorflow")
@@ -46,23 +56,26 @@ class Predictor:
     def load_generic_model(self, name):
         """
         Function that loads the generic model for fuel scaling:
+
         :return: The tensorflow model
         """
         try:
-            path = self.pkg_resources.resource_filename('acropole', name)
+            path = self.pkg_resources.resource_filename("acropole", name)
         except FileNotFoundError:
             path = name
-        return self.tf.saved_model.load(path)
+        return self.tf.keras.models.load_model(path, compile=False)
 
     def predict_fuel_generic(self, model_input_values):
         """
         Function that predicts the generic fuel flow from a list of input values
+
         :param model_input_values: is the array of inputs:
         :return: List of predicted generic fuel flow
         """
         model = self.cache(self.model_name)
-        model_input_values = (model_input_values - self.fuel_min) / (self.fuel_max - self.fuel_min)
+        model_input_values = (model_input_values - self.fuel_min) / (
+            self.fuel_max - self.fuel_min
+        )
         data = self.tf.constant(model_input_values, dtype=self.tf.float32)
-        tensorflow_result_dict = model.signatures[self.tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY](data)
-        key, values = tensorflow_result_dict.popitem()
-        return list(values.numpy().flatten())
+        values = model.predict(data)
+        return list(values.flatten())
--- acropole/trajectory.py
+++ acropole/trajectory.py
@@ -2,12 +2,19 @@
 #  @author: Gabriel JARRY
 # @endinternal
 
-from .columns import COLS_SMOOTH, COLS_KEEP, COLS_RESAMPLE, COLS_ACFT_PARAMS, COLS_PROCESS, \
-    COLS_INPUT_FUEL, COLS_FUEL, COL_ACFT_ICAO_TYPE
-
+import importlib
 from datetime import timedelta
 
-import importlib
+from .columns import (
+    COL_ACFT_ICAO_TYPE,
+    COLS_ACFT_PARAMS,
+    COLS_FUEL,
+    COLS_INPUT_FUEL,
+    COLS_KEEP,
+    COLS_PROCESS,
+    COLS_RESAMPLE,
+    COLS_SMOOTH,
+)
 
 
 class Trajectory:
@@ -24,23 +31,31 @@ class Trajectory:
     __ARR = "arr"
     __DERIV = "DERIV"
     __SEP = "_"
-    __BFILL = "bfill"
-    __LEFT = "left"
 
     __ACFT_DICT_PATH = "data/acft_params.csv"
 
     __DEFAULT_MASS = -1.0
 
-    def __init__(self, sample_rate=4.0, min_duration_s=20, min_conf_ind=0.8,
-                 mock_scipy=None, mock_pkg_resources=None, mock_predictor=None, mock_utils=None):
+    def __init__(
+        self,
+        sample_rate=4.0,
+        min_duration_s=20,
+        min_conf_ind=0.8,
+        mock_scipy=None,
+        mock_pkg_resources=None,
+        mock_predictor=None,
+        mock_utils=None,
+    ):
         """
         Class initilizer that load dependant packages, and inits model cache.
+
         :param sample_rate: Sample rate for resample methods as a Float (default 20s)
         :param min_duration_s: Minimum time duration for segment cco/cdo as a Float (default 20s)
         :param min_conf_ind: Minimum confidence index to compute fuel flow using Acropole Lib (Default is 0.8)
         :param mock_scipy: mock for scipy package (Default is None)
         :param mock_predictor: mock for predictor package (Default is None)
         :param mock_utils: mock for acropole.utils package (Default is None)
+
         """
         self.sample_rate = sample_rate
         self.min_duration_s = min_duration_s
@@ -71,7 +86,13 @@ class Trajectory:
 
         self.acft_dict = self.load_generic_acft_dict(self.__ACFT_DICT_PATH)
 
-    def resample(self, df, cols_keep=COLS_KEEP, cols_resample=COLS_RESAMPLE, cols_process=COLS_PROCESS):
+    def resample(
+        self,
+        df,
+        cols_keep=COLS_KEEP,
+        cols_resample=COLS_RESAMPLE,
+        cols_process=COLS_PROCESS,
+    ):
         """
         Function that resamples trajectory.
         :param df: trajectory as a Pandas Dataframe
@@ -80,12 +101,22 @@ class Trajectory:
         :param cols_process: Names of columns for process as a List of String (default COL_PROCESS)
         :return: The resampled trajectory as a Pandas Dataframe
         """
-        col_syst_point_id, col_time, col_time_last_plot, col_flight_time, col_plot_date = cols_process
-        df[col_time] = self.pd.to_datetime(df[col_plot_date], infer_datetime_format=True)
+        (
+            col_syst_point_id,
+            col_time,
+            col_time_last_plot,
+            col_flight_time,
+            col_plot_date,
+        ) = cols_process
+
+        df[col_time] = self.pd.to_datetime(df[col_plot_date])
         df = df.sort_values(by=col_time)
         df[col_time_last_plot] = df[col_time] - df[col_time].shift(1)
-        df[col_time_last_plot] = df[col_time_last_plot].apply(lambda dt: dt.seconds + dt.microseconds / 10e5).\
-            fillna(0.0)
+        df[col_time_last_plot] = (
+            df[col_time_last_plot]
+            .apply(lambda dt: dt.seconds + dt.microseconds / 10e5)
+            .fillna(0.0)
+        )
         df[col_flight_time] = df[col_time_last_plot].cumsum()
 
         start = df[col_flight_time].iloc[0]
@@ -97,9 +128,11 @@ class Trajectory:
             res_df[col] = [df[col].iloc[0]] * len(r)
 
         res_df[col_flight_time] = r
-        res_df[col_time_last_plot] = [0.0] + [4.0] * (len(r)-1)
-        res_df[col_syst_point_id] = range(1, len(res_df)+1)
-        res_df[col_plot_date] = df.TIME.iloc[0] + res_df[col_flight_time].apply(lambda el: timedelta(seconds=el))
+        res_df[col_time_last_plot] = [0.0] + [4.0] * (len(r) - 1)
+        res_df[col_syst_point_id] = range(1, len(res_df) + 1)
+        res_df[col_plot_date] = df.TIME.iloc[0] + res_df[col_flight_time].apply(
+            lambda el: timedelta(seconds=el)
+        )
         res_df[col_plot_date] = res_df[col_plot_date].apply(str)
 
         for col in cols_resample:
@@ -126,14 +159,16 @@ class Trajectory:
         :return: The aircracft parameters dict
         """
         try:
-            path = self.pkg_resources.resource_filename('acropole', name)
+            path = self.pkg_resources.resource_filename("acropole", name)
         except FileNotFoundError:
             path = name
         acft_df = self.pd.read_csv(path, sep=";")
         acft_dict = {row[COL_ACFT_ICAO_TYPE]: row for key, row in acft_df.iterrows()}
         return acft_dict
 
-    def fuel_prediction(self, df, acft_dict, cols_fuel=COLS_FUEL, mass_kg=None, tas=None):
+    def fuel_prediction(
+        self, df, acft_dict, cols_fuel=COLS_FUEL, mass_kg=None, tas=None
+    ):
         """
         Function that computes derivates and predict fuel consumption over trajectory using Acropole Librairy
         :param df: trajectory as a Pandas Dataframe
@@ -143,9 +178,25 @@ class Trajectory:
         :param tas: True Air Speed column name or None if no mass as a String
         :return: The current trajectory with extra fuel information as a Pandas Dataframe
         """
-        col_estim_fuel_flow_acrpl, col_estim_conso_kg, col_estim_fuel_flow_kgh, col_grnd_spd_kt, col_true_air_spd_kt, \
-            col_mass, col_flpl_airc_type, col_engine_num, col_fuel_flow_to, col_conf_ind, col_deriv_tas_kt, \
-            col_deriv_gs_kt, col_time_last_plot, col_alti_std_ft, col_oew, col_mtow = cols_fuel
+        (
+            col_estim_fuel_flow_acrpl,
+            col_estim_conso_kg,
+            col_estim_fuel_flow_kgh,
+            col_grnd_spd_kt,
+            col_true_air_spd_kt,
+            col_mass,
+            col_flpl_airc_type,
+            col_engine_num,
+            col_fuel_flow_to,
+            col_conf_ind,
+            col_deriv_tas_kt,
+            col_deriv_gs_kt,
+            col_time_last_plot,
+            col_alti_std_ft,
+            col_oew,
+            col_mtow,
+        ) = cols_fuel
+
         acft_type = df[col_flpl_airc_type].iloc[0]
         if tas:
             df.loc[:, col_true_air_spd_kt] = df.loc[:, tas]
@@ -153,8 +204,9 @@ class Trajectory:
             df.loc[:, col_true_air_spd_kt] = df[col_grnd_spd_kt]
 
         for col in [col_alti_std_ft, col_grnd_spd_kt, col_true_air_spd_kt]:
-            df.loc[:, self.__DERIV + self.__SEP + col] = (df[col] - df[col].shift(1)). \
-                                               fillna(method=self.__BFILL) / df[col_time_last_plot]
+            df.loc[:, self.__DERIV + self.__SEP + col] = (
+                df[col] - df[col].shift(1)
+            ).bfill() / df[col_time_last_plot]
 
         if acft_type in acft_dict.keys():
             params = acft_dict[acft_type]
@@ -163,20 +215,30 @@ class Trajectory:
                 df.loc[:, col] = params[col]
 
             if mass_kg:
-                df.loc[:, col_mass] = (df.loc[:, mass_kg] - df[col_oew]) / (df[col_mtow] - df[col_oew])
+                df.loc[:, col_mass] = (df.loc[:, mass_kg] - df[col_oew]) / (
+                    df[col_mtow] - df[col_oew]
+                )
             else:
                 df.loc[:, col_mass] = self.__DEFAULT_MASS
 
             input_values = df[COLS_INPUT_FUEL]
 
             if params[col_conf_ind] > self.min_conf_ind:
-                df.loc[:, col_estim_fuel_flow_acrpl] = self.predictor.predict_fuel_generic(input_values)
+                df.loc[:, col_estim_fuel_flow_acrpl] = (
+                    self.predictor.predict_fuel_generic(input_values)
+                )
             else:
                 df.loc[:, col_estim_fuel_flow_acrpl] = None
 
-            df.loc[:, col_estim_fuel_flow_kgh] = (df[col_estim_fuel_flow_acrpl] * df[col_engine_num] *
-                                                  df[col_fuel_flow_to] * self.__HOUR)
-            df.loc[:, col_estim_conso_kg] = df[col_estim_fuel_flow_kgh] * df[col_time_last_plot] / self.__HOUR
+            df.loc[:, col_estim_fuel_flow_kgh] = (
+                df[col_estim_fuel_flow_acrpl]
+                * df[col_engine_num]
+                * df[col_fuel_flow_to]
+                * self.__HOUR
+            )
+            df.loc[:, col_estim_conso_kg] = (
+                df[col_estim_fuel_flow_kgh] * df[col_time_last_plot] / self.__HOUR
+            )
         else:
             for col in COLS_ACFT_PARAMS[1:]:
                 df.loc[:, col] = None
@@ -186,8 +248,16 @@ class Trajectory:
             df.loc[:, col_estim_conso_kg] = None
         return df
 
-    def trajectory_process(self, df, cols_keep=COLS_KEEP, cols_resample=COLS_RESAMPLE, cols_smooth=COLS_SMOOTH,
-                           mass_kg=None, tas=None, apply_smoothing=False):
+    def trajectory_process(
+        self,
+        df,
+        cols_keep=COLS_KEEP,
+        cols_resample=COLS_RESAMPLE,
+        cols_smooth=COLS_SMOOTH,
+        mass_kg=None,
+        tas=None,
+        apply_smoothing=False,
+    ):
         """
         Function that preprocess trajectory Dataframe with resampling, moving average smoothing, adding distance and
         fuel along trajectory.
